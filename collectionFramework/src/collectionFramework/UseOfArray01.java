f8e1e860a10a31233ac2ff14030b01e90034e73f
tikly <ttaekymm@gmail.com>
2020-02-12 12:52:50 -0500

    My updated code

--- collectionFramework/src/collectionFramework/UseOfArray01.java
+++ collectionFramework/src/collectionFramework/UseOfArray01.java
@@ -3,17 +3,18 @@ package collectionFramework;
 public class UseOfArray01 {
 
 	public static void main(String[] args) {
-		int [] a =new int[6];
+		int [] a =new int[8];
 		a[0]=10;
 		a[1]=20;
 		a[2]=30;
 		a[4]=40;
-		//a[6]=60;
+		a[6]=60;
 		System.out.println("Printing single index value "+a[0]);
 		System.out.println("Printing single index value "+a[1]);
 		System.out.println("Printing single index value "+a[2]);
 		System.out.println("Printing single index value "+a[4]);
 		System.out.println("Printing single index value "+a[5]);
-		//System.out.println("Printing single index value "+a[6]);
+		System.out.println("Printing single index value "+a.length);//it will show the total number of size
+		System.out.println("Printing single index value "+(a.length-2));//it will deduct 2 from total number of size of array
 		}
 }
--- collectionFramework/src/collectionFramework/UseOfArrayprac01.java
+++ collectionFramework/src/collectionFramework/UseOfArrayprac01.java
@@ -10,11 +10,11 @@ public class UseOfArrayprac01 {
 		i[3]=20;
 		i[4]=25;
 		//i[5]=30;
-		System.out.println("The first value is"+i[0]);
-		System.out.println("The second value is"+i[1]);
-		System.out.println("The thried value is"+i[3]);
-		System.out.println("The fourth value is"+i[4]);
-		System.out.println("The fifth value is"+i[5]);
+		System.out.println("The first value is "+i[0]);
+		System.out.println("The second value is "+i[1]);
+		System.out.println("The thried value is "+i[3]);
+		System.out.println("The fourth value is "+i.length);
+		System.out.println("The fifth value is "+(i.length-1));
 		//System.out.println("The sixth value is"+i);
 		}
 
