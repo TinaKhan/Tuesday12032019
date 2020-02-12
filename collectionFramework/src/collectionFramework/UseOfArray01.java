efe1052a3c0dae01587a2ab9e61ecc57d79f2b77
tikly <ttaekymm@gmail.com>
2020-02-12 12:30:54 -0500

    My wednesday's commit

---
+++ collectionFramework/src/collectionFramework/UseOfArray01.java
@@ -0,0 +1,19 @@
+package collectionFramework;
+
+public class UseOfArray01 {
+
+	public static void main(String[] args) {
+		int [] a =new int[6];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[4]=40;
+		//a[6]=60;
+		System.out.println("Printing single index value "+a[0]);
+		System.out.println("Printing single index value "+a[1]);
+		System.out.println("Printing single index value "+a[2]);
+		System.out.println("Printing single index value "+a[4]);
+		System.out.println("Printing single index value "+a[5]);
+		//System.out.println("Printing single index value "+a[6]);
+		}
+}
---
+++ collectionFramework/src/collectionFramework/UseOfArrayprac02.java
@@ -0,0 +1,22 @@
+package collectionFramework;
+
+public class UseOfArrayprac02 {
+
+	public static void main(String[] args) {
+		int []a=new int [7];
+		a[0]=4;
+		a[1]=8;
+		a[2]=12;
+		a[3]=16;
+		a[4]=20;
+		a[4]=25;
+	System.out.println("The printing value of 1st is :"+a[0]);
+	System.out.println("The printing value of 2nd is :"+a[1]);
+	System.out.println("The printing value of 3rd is :"+a[2]);
+	System.out.println("The printing value of 4th is :"+a[3]);
+	System.out.println("The printing value of 5th is :"+a[4]);
+	System.out.println("The printing value of 6th is :"+a[5]);
+	}
+	
+
+}
